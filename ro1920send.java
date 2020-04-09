79f1bb7365c6a81ca4f30efd7aa40ee089719a88
Adrián Alonso <adrianalonsovilar@gmail.com>
2020-04-09 12:39:35 +0200

    A

--- ro1920send.java
+++ ro1920send.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 
 public class ro1920recv {
   public static void main(String[] args) {
+    String
 
+    try {
+      Scanner input_file = new Scanner(new File);
+    } catch(Exception e) {
+
+    }
   }
 
 }
