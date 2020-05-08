23d05654c0b15ca4f734fe8fb35b32fd0cb48522
Adrián Alonso <adrianalonsovilar@gmail.com>
2020-05-08 23:05:49 +0200

    APP_v.3

--- send/Send.java
+++ send/Send.java
@@ -92,17 +92,29 @@ public class Send {
   }
 
   private byte[] getIP(String IP_text){
-    String[] IP_data = IP_text.split("\\.");
-    String data0 = IP_data[0];
-    String data1 = IP_data[1];
-    String data2 = IP_data[2];
-    String data3 = IP_data[3];
-    byte[] IP0 = ByteBuffer.allocate(2).putShort(Short.parseShort(data0)).array();
-    byte[] IP1 = ByteBuffer.allocate(2).putShort(Short.parseShort(data1)).array();
-    byte[] IP2 = ByteBuffer.allocate(2).putShort(Short.parseShort(data2)).array();
-    byte[] IP3 = ByteBuffer.allocate(2).putShort(Short.parseShort(data3)).array();
-    byte[] IP_bytes = ByteBuffer.allocate(4).put(IP0[1]).put(IP1[1])
-      .put(IP2[1]).put(IP3[1]).array();
+    byte[] IP_bytes;
+    byte[] IP0;
+    byte[] IP1;
+    byte[] IP2;
+    byte[] IP3;
+    byte IP_0 = 127;
+    byte IP_1 = 0;
+    byte IP_2 = 0;
+    byte IP_3 = 1;
+    if(IP_text.equals("localhost")){
+      IP_bytes = ByteBuffer.allocate(4).put(IP_0).put(IP_1).put(IP_2).put(IP_3).array();
+    }else{
+      String[] IP_data = IP_text.split("\\.");
+      String data0 = IP_data[0];
+      String data1 = IP_data[1];
+      String data2 = IP_data[2];
+      String data3 = IP_data[3];
+      IP0 = ByteBuffer.allocate(2).putShort(Short.parseShort(data0)).array();
+      IP1 = ByteBuffer.allocate(2).putShort(Short.parseShort(data1)).array();
+      IP2 = ByteBuffer.allocate(2).putShort(Short.parseShort(data2)).array();
+      IP3 = ByteBuffer.allocate(2).putShort(Short.parseShort(data3)).array();
+      IP_bytes = ByteBuffer.allocate(4).put(IP0[1]).put(IP1[1]).put(IP2[1]).put(IP3[1]).array();
+    }
 
     return IP_bytes;
   }
@@ -126,7 +138,7 @@ public class Send {
     int ack_int_recv;
     int ack_int = ((ack_num[0] & 0xFF) << 24) | ((ack_num[1] & 0xFF) << 16) | ((ack_num[2] & 0xFF) << 8) | ((ack_num[3] & 0xFF) <<0);
     byte[] ack_recv;
-    int time=50;
+    int time=45;
     boolean timeout;
 
     do {
@@ -157,7 +169,7 @@ public class Send {
 
       timeout=false;
 
-  } while ((ack_int_recv!=ack_int) || (timeout==true));
+    } while ((ack_int_recv!=ack_int) || (timeout==true));
 
     datagramSocket.close();
 
